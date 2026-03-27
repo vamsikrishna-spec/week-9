d18d57307f14b0a9d9b88e0841da59faeb54a293
vamsikrishna-spec <24331a05e7@mvgrce.edu.in>
2026-03-27 11:04:35 +0530

    Add files via upload

---
+++ vamsi.py
@@ -0,0 +1,20 @@
+
+file = open("sample.txt", "w")
+file.write("vamsi\n")
+lines = ["vamsi\n", "D.O.B-17-11-2005\n", "College - MVGR\n"]
+file.writelines(lines)
+file.close()
+file = open("sample.txt", "r")
+print("read()\n:")
+print(file.read())     
+file.close()
+file = open("sample.txt", "r")
+print("\nreadline()\n:")
+print(file.readline()) 
+print(file.readline()) 
+file.close()
+
+file = open("sample.txt", "r")
+print("\nreadlines():")
+print(file.readlines()) 
+file.close()
